ad5b08e5c99f95b243bee1e883b9e1a9db640e49
Chen Chen <52281005+randomgoose@users.noreply.github.com>
2021-10-23 13:56:27 +0800

    add pie chart

--- src/app/components/DimensionIndicator.tsx
+++ src/app/components/DimensionIndicator.tsx
@@ -13,10 +13,10 @@ export default function DimensionIndicator({width, height}: Props) {
     return (
         <div className={'DimensionIndicator absolute top-6 left-2/4 transform -translate-x-1/2 -translate-y-1/2'}>
             <span className={'mr-2'}>
-                {t('Width')}: {width}
+                {t('Width')}: <span className={'font-bold'}>{width}</span>
             </span>
             <span>
-                {t('Height')}: {height}
+                {t('Height')}: <span className={'font-bold'}>{height}</span>
             </span>
         </div>
     );
